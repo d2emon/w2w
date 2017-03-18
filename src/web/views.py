5e0e4e8b0dfb2143ab7967285a539fdc6362ef23
Dmitry Kutsenko <d2emonium@gmail.com>
2017-03-18 17:04:00 +0400

    Templates

--- src/web/views.py
+++ src/web/views.py
@@ -1,7 +1,23 @@
+from flask import render_template
 from web import app
 
 
 @app.route("/")
 @app.route("/index")
 def index():
-    return "Hello world!!!"
+    user = {'nickname': "D2emon", }
+    posts = [
+             {
+              'author': {'nickname': "John"},
+              'body': "Beautyfull day in Portland!",
+              },
+             {
+              'author': {'nickname': "Susan"},
+              'body': "The Avengers movie was so cool!",
+              },
+             ]
+    return render_template("index.html", 
+                           title="Home",
+                           user=user,
+                           posts=posts,
+                           )
