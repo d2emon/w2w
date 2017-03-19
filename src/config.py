a02f943ef632fa70febfd4035a74f4f92373cca8
Dmitry Kutsenko <d2emonium@gmail.com>
2017-03-19 11:48:02 +0300

    Authorization

--- src/config.py
+++ src/config.py
@@ -5,12 +5,13 @@ basedir = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
 CSRF_ENABLED = True
 SECRET_KEY = 'ThereIsNoSpoon'
 OPENID_PROVIDERS = [
-                    {'name': "Google", 'url': "https://www.google.com/accounts/o8/id"},
-                    {'name': "Yahoo", 'url': "https://me.yahoo.com"},
-                    {'name': "AOL", 'url': "https://openid.aol.com/<username>"},
-                    {'name': "Flickr", 'url': "https://www.flickr.com/<username>"},
-                    {'name': "MyOpenID", 'url': "https://www.myopenid.com"},
-                    ]
+    # {'name': "Google", 'url': "https://www.google.com/accounts/o8/id"},
+    {'name': "Yahoo", 'url': "https://me.yahoo.com"},
+    {'name': "AOL", 'url': "https://openid.aol.com/<username>"},
+    {'name': "Flickr", 'url': "https://www.flickr.com/<username>"},
+    {'name': "MyOpenID", 'url': "https://www.myopenid.com"},
+]
+# OIDC_CLIENT_SECRETS = os.path.join(basedir, "secrets")
 SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, "db", "w2w.db")
 # SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, "migrations")
 SQLALCHEMY_TRACK_MODIFICATIONS = True
--- src/web/views.py
+++ src/web/views.py
@@ -1,23 +1,26 @@
-from flask import render_template, flash, redirect
-from web import app
+from flask import g, session, request, url_for, render_template, redirect, flash
+from flask_login import current_user, login_user, login_required, logout_user
+from web import app, db, oid, lm
 from .forms import LoginForm
+from .models import User, ROLE_USER
 
 
 @app.route("/")
 @app.route("/index")
+@login_required
 def index():
-    user = {'nickname': "D2emon", }
+    user = g.user
     posts = [
-             {
-              'author': {'nickname': "John"},
-              'body': "Beautyfull day in Portland!",
-              },
-             {
-              'author': {'nickname': "Susan"},
-              'body': "The Avengers movie was so cool!",
-              },
-             ]
-    return render_template("index.html", 
+        {
+            'author': {'nickname': "John"},
+            'body': "Beautyfull day in Portland!",
+        },
+        {
+            'author': {'nickname': "Susan"},
+            'body': "The Avengers movie was so cool!",
+        },
+    ]
+    return render_template("index.html",
                            title="Home",
                            user=user,
                            posts=posts,
@@ -25,12 +28,56 @@ def index():
 
 
 @app.route('/login', methods=['GET', 'POST', ])
+@oid.loginhandler
 def login():
+    # https://openid-provider.appspot.com/d2emonium
+    if g.user is not None and g.user.is_authenticated:
+        return redirect(url_for('index'))
+
     form = LoginForm()
     if form.validate_on_submit():
-        flash('Login requested for OpenID="' + form.openid.data + '", remember_me=' + str(form.remember_me.data))
-        return redirect('/index')
+        session['remember_me'] = form.remember_me.data
+        # return oidc.user_getfield('email')
+        return oid.try_login(form.openid.data, ask_for=['nickname', 'email'])
+        # flash('Login requested for OpenID="' + form.openid.data + '", remember_me=' + str(form.remember_me.data))
     return render_template('login.html',
                            title='Sign In',
                            form=form,
                            providers=app.config['OPENID_PROVIDERS'])
+
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
+
+
+@app.before_request
+def before_request():
+    g.user = current_user
+
+
+@oid.after_login
+def after_login(resp):
+    if resp.email is None or resp.email == "":
+        flash("Invalid login. Please try again.")
+        return redirect(url_for('login'))
+    user = User.query.filter_by(email=resp.email).first()
+    if user is None:
+        nickname = resp.nickname
+        if nickname is None or nickname == "":
+            nickname = resp.email.split('@')[0]
+        user = User(nickname=nickname, email=resp.email, role=ROLE_USER)
+        db.session.add(user)
+        db.session.commit()
+    remember_me = False
+    if 'remember_me' in session:
+        remember_me = session['remember_me']
+        session.pop('remember_me', None)
+    login_user(user, remember=remember_me)
+    return redirect(request.args.get('next') or url_for('index'))
+
+
+@lm.user_loader
+def load_user(id):
+    return User.query.get(int(id))
