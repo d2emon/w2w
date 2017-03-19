4821b8ea8cb6073d987c111ca24a82b13a320a0c
Dmitry Kutsenko <d2emonium@gmail.com>
2017-03-19 18:15:40 +0300

    Logging

--- src/config.py
+++ src/config.py
@@ -15,3 +15,10 @@ OPENID_PROVIDERS = [
 SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, "db", "w2w.db")
 # SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, "migrations")
 SQLALCHEMY_TRACK_MODIFICATIONS = True
+
+MAIL_SERVER = 'localhost'
+MAIL_PORT = 25
+MAIL_USERNAME = None
+MAIL_PASSWORD = None
+
+ADMINS = ['admin@example.com', ]
--- src/web/__init__.py
+++ src/web/__init__.py
@@ -4,10 +4,27 @@ from flask_migrate import Migrate
 from flask_login import LoginManager
 from flask_openid import OpenID
 import os
-from config import basedir
+from config import basedir, ADMINS, MAIL_SERVER, MAIL_PORT, MAIL_USERNAME, MAIL_PASSWORD
 
 
 app = Flask(__name__)
+log = True  # not app.debug
+if log:
+    import logging
+    from logging.handlers import SMTPHandler, RotatingFileHandler
+    credentials = None
+    if MAIL_USERNAME or MAIL_PASSWORD:
+        credentials = (MAIL_USERNAME, MAIL_PASSWORD)
+    mail_handler = SMTPHandler((MAIL_SERVER, MAIL_PORT), 'no-reply@' + MAIL_SERVER, ADMINS, 'microblog failure', credentials)
+    mail_handler.setLevel(logging.ERROR)
+    app.logger.addHandler(mail_handler)
+
+    file_handler = RotatingFileHandler(os.path.join(basedir, 'log/w2w.log'), 'a', 1 * 1024 * 1024, 10)
+    file_handler.setFormatter(logging.Formatter('%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'))
+    app.logger.setLevel(logging.INFO)
+    file_handler.setLevel(logging.INFO)
+    app.logger.addHandler(file_handler)
+    app.logger.info('w2w startup')
 app.config.from_object('config')
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
