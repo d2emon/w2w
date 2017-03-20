e4a9af016596f8a84c06b323be33ad346b607220
Dmitry Kutsenko <d2emonium@gmail.com>
2017-03-20 09:28:22 +0300

    Testing

---
+++ src/test.py
@@ -0,0 +1,31 @@
+#! /usr/bin/env python
+import os
+import unittest
+
+
+from config import basedir
+from web import app, db
+from web.models import User
+
+
+class TestCase(unittest.TestCase):
+    def setUp(self):
+        app.config['TESTING'] = True
+        app.config['CSRF_ENABLED'] = False
+        app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'db/', 'test.db')
+        self.app = app.test_client()
+        db.create_all()
+
+    def tearDown(self):
+        db.session.remove()
+        db.drop_all()
+
+    def test_avatar(self):
+        u = User(nickname='john', email='john@example.com')
+        avatar = u.avatar(128)
+        expected = 'http://www.gravatar.com/avatar/d4c74594d841139328695756648b6bd6'
+        assert avatar[0:len(expected)] == expected
+
+
+if __name__ == '__main__':
+    unittest.main()
