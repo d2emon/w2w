f58be8a225a5acabdbce51211b176bcf5c756427
Dmitry Kutsenko <d2emonium@gmail.com>
2017-03-21 16:17:48 +0300

    Test debugging

--- src/test.py
+++ src/test.py
@@ -42,6 +42,35 @@ class TestCase(unittest.TestCase):
         assert nickname2 != 'john'
         assert nickname2 != nickname
 
+    def test_follow(self):
+        u1 = User(nickname='john', email='john@example.com')
+        u2 = User(nickname='susan', email= 'susan@example.com')
+        db.session.add(u1)
+        db.session.add(u2)
+        db.session.commit()
+        print(u1, u2)
+        print(u1.followers)
+        print(u1.unfollow(u2))
+        assert u1.unfollow(u2) is None
+
+        u = u1.follow(u2)
+        db.session.add(u)
+        db.sesion.commit()
+        assert u1.follow(u2) is None
+        assert u1.is_following(u2)
+        assert u1.followed.count() == 1
+        assert u1.followed.first().nickname == 'susan'
+        assert u2.followers.count() == 1
+        assert u2.followers.first().nickname == 'susan'
+
+        u = u1.unfollow(u2)
+        assert u is not None
+        db.session.add(u)
+        db.session.commit()
+        assert u1.is_following(u2) is False
+        assert u1.followed.count() == 0
+        assert u2.followers.count() == 0
+
 
 if __name__ == '__main__':
     unittest.main()
