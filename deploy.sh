29d23bd47cb8a77b61171b37e56134cf77f9e2d0
Dmitry Kutsenko <d2emonium@gmail.com>
2017-03-27 17:13:53 +0300

    Comments  in deploy

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,17 @@
 #! /bin/bash
+echo "Installing app"
 git pull
+echo "Installing pip dependencies"
 pip install -r requirements.txt
+echo "Installing bower dependencies"
 bower install
+echo "Change dir"
 cd src
 export FLASK_APP=run_server.py
+echo "Setting up FLASK_APP to '$FLASK_APP'"
+echo "Upgrading db"
 flask db upgrade head
 # pybabel extract -F babel.cfg -o messages.pot web
 # pybabel update -i messages.pot -d web/translations
+echo "Compiling translations"
 pybabel compile -d web/translations
