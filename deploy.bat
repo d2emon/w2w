edf4b7e515205407f4c6b0f3e494664d9f79a7fb
Dmitry Kutsenko <d2emonium@gmail.com>
2017-04-02 00:25:35 +0300

    Babel config

--- deploy.bat
+++ deploy.bat
@@ -2,14 +2,14 @@ echo "Installing app"
 git pull
 echo "Installing pip dependencies"
 pip install -r requirements.txt
+echo "Compiling translations"
+pybabel compile -d web\translations
 echo "Change dir"
 cd src
 set FLASK_APP=run_server.py
 echo "Setting up FLASK_APP to '%FLASK_APP%'"
 echo "Upgrading db"
 flask db upgrade head
-echo "Compiling translations"
-pybabel compile -d web\translations
 cd ..
 echo "Installing bower dependencies"
 bower install
